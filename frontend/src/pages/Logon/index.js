97228a0d9734754c15a1aa35395c70794ea3643f
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-03-25 19:59:11 -0300

    Estilização da página de registro e algumas alterações no estilo da página de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -21,7 +21,7 @@ export default function Logon() {
             Entrar
           </button>
 
-          <Link to="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
           </Link>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -22,7 +22,7 @@ export default function Register() {
 
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#e02041" />
-            Não tenho cadastro
+            Voltar para o Logon
           </Link>
         </section>
 
