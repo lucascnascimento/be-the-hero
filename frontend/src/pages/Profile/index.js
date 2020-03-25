f3fae466ef791cc58698a8387733572fd55e1436
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-03-25 20:12:35 -0300

    estrutura do header criada

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem Vinda, APAD</span>
+
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+
+        <button type="button">
+          <FiPower size={18} color="#e02041" />
+        </button>
+      </header>
+    </div>
+  );
+}
