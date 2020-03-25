f31c67a070f94a6cab4e8394b85ca9e2cbb4113a
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-03-25 19:46:44 -0300

    Criação da estrutura html da página register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
+import logoImg from "../../assets/logo.svg";
+import { FiArrowLeft } from "react-icons/fi";
+
 export default function Register() {
-  return <h1>Register</h1>;
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastro</h1>
+
+          <p>
+            Faça o seu cadastro, entre na plataforma e ajude as pessoas a
+            encontrarem os casos da sua ONG
+          </p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form action="">
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input placeholder="WhatsApp" />
+
+          <div className="input-group">
+            <input placeholder="Cidade" />
+            <input placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
