0275fe364379f6018ddc67ec39629e258015b484
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-03-26 20:48:39 -0300

    Navegação para página principal implementada

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { View, TouchableOpacity, Image, Text } from "react-native";
 
 import logoImg from "../../assets/logo.png";
@@ -7,12 +8,18 @@ import logoImg from "../../assets/logo.png";
 import styles from "./styles";
 
 export default function Detail() {
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
 
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={navigateBack}>
           <Feather name="arrow-left" size={28} color="#e02041" />
         </TouchableOpacity>
       </View>
